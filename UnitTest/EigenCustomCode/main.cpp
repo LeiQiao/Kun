e49949fc6a1fb8c4532865c165e11dd654c83239
LeiQiao <lei.qiao@leiqiaodeMacBook-Pro.local>
2017-03-24 13:26:43 +0800

    fix unit test bug

--- UnitTest/EigenCustomCode/main.cpp
+++ UnitTest/EigenCustomCode/main.cpp
@@ -8,7 +8,7 @@ int main()
     memcpy(input.data(), uint_test_data::unit_test_data, %DIM_SIZE_1%*%DIM_SIZE_2%*%DIM_SIZE_3%*sizeof(%CPP_TYPE%));
     
     Eigen::Tensor<%CPP_TYPE%, 1, Eigen::RowMajor> output;
-    idcard_number::predict(input, output, true);
+    %MODEL_NAME%::predict(input, output, true);
 
     FILE* file = fopen("./unit_test_input.txt", "wb");
     for( int i=0; i<%DIM_SIZE_1%*%DIM_SIZE_2%*%DIM_SIZE_3%; i++ )
